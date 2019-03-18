cdc29ba10560a0d174d527a93dcf00e568f3bc65
Andrew Soden <andrewsoden@hotmail.com>
2019-03-18 19:39:50 +1000

    Update basic navigation test

--- cypress/integration/basic_navigation.js
+++ cypress/integration/basic_navigation.js
@@ -4,22 +4,22 @@ describe('Toast Recipes basic page navigation', function() {
     })
 
     it('visits the Information page', function() {
-        cy.get(':nth-child(2) > a').click()
+        cy.get('#section_information').click()
         cy.get('h2').contains('Rejoice for this is the Information page')
     })
 
     it('visits the Contact page', function() {
-        cy.get(':nth-child(3) > a').click()
+        cy.get('#section_contact').click()
         cy.get('h1').contains('Contact')
     })
 
     it('visits the Login page', function() {
-        cy.get(':nth-child(4) > a').click()
+        cy.get('#section_login').click()
         cy.get('h2').contains('Rejoice for this is the Login page')
     })
 
     it('visits the Home page', function() {
-        cy.get(':nth-child(1) > a').click()
+        cy.get('#section_home').click()
         cy.get('h2').contains('Rejoice for this is the Home page')
     })
 })
